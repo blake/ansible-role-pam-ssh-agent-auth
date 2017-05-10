369d0ff1f76e5b7ec67f260a61e9431b91e0ee60
juju4 <juju4@users.noreply.github.com>
2017-05-09 20:51:37 -0400

    add serverspec+bats tests

---
+++ test/integration/default/serverspec/pamsshagent_spec.rb
@@ -0,0 +1,28 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+## Use Junit formatter output, supported by jenkins
+#require 'yarjuf'
+#RSpec.configure do |c|
+#    c.formatter = 'JUnit'
+#end
+
+describe package('sudo') do
+  it { should be_installed }
+end
+describe package('pam-ssh-agent-auth') do
+  it { should be_installed }
+end
+
+describe file('/etc/sudoers') do
+  it { should be_file }
+  its(:content) { should match /Defaults env_keep \+= SSH_AUTH_SOCK/ }
+end
+
+describe file('/etc/pam.d/sudo') do
+  it { should be_file }
+  its(:content) { should match /auth sufficient pam_ssh_agent_auth\.so file=%h\/.ssh\/authorized_keys/ }
+end
+
