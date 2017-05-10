85365883b68892c6804d027c5f7d98df831769d8
juju4 <juju4@users.noreply.github.com>
2017-05-10 07:56:54 -0400

    serverspec match: check as root

--- test/integration/default/serverspec/pamsshagent_spec.rb
+++ test/integration/default/serverspec/pamsshagent_spec.rb
@@ -18,9 +18,8 @@ end
 
 describe file('/etc/sudoers') do
   it { should be_file }
-#  its(:content) { should match /Defaults env_keep += SSH_AUTH_SOCK/ }
-  its(:content) { should match /Defaults env_keep / }
-  its(:content) { should match /SSH_AUTH_SOCK/ }
+  its(:content) { should match /Defaults env_keep \+= SSH_AUTH_SOCK/ }
+  let(:sudo_options) { '-u root -H' }
 end
 
 describe file('/etc/pam.d/sudo') do
